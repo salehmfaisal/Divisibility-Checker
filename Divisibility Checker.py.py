a3afa045f9bccda52817ed0b5ad7ef965c322458
salehmfaisal <58755264+salehmfaisal@users.noreply.github.com>
2021-03-06 15:39:09 -0700

    Add files via upload
    This small program will check the divisibility of an integer by 3 and 5 and will return Yes if divisible and No for not divisible.

---
+++ Divisibility Checker.py.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #User will input an integer. If this number is divisable by 3 and 5 program will show Yes as out otherwise No.
+
+# In[5]:
+
+
+number= int(input("Please input an integer:" ))
+
+if number%3==0 and number%5==0:
+    print("Yes")
+else:
+    print("No")
+
+
+# In[ ]:
+
+
+
+
